c6db9850da3128f73b56b158d143c5041f177004
Mohit Sai <147088949+IMohitSai@users.noreply.github.com>
2025-02-10 13:44:29 +0530

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,16 @@
+import pyautogui
+from datetime import datetime
+
+# Set the screenshot filename format
+filename_format = "screenshot_{}"
+
+while True:
+    # Take a screenshot
+    image = pyautogui.screenshot()
+
+    # Get the current timestamp to use in the filename
+    now = datetime.now()
+    formatted_timestamp = now.strftime("%Y-%m-%d_%H:%M:%S")
+
+    # Save the screenshot with the current timestamp as part of its name
+    image.save(f"{filename_format.format(formatted_timestamp)}.png")
